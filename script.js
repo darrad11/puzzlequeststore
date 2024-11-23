99300cccc82dfc0fb739a3bfd8b24246c69e8652
darrad11 <127601753+darrad11@users.noreply.github.com>
2024-11-23 09:10:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,18 +18,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const nextButton = document.querySelector('.next');
         const dots = document.querySelectorAll('.dot');
         
-        // Funkcja do wyświetlania slajdów
-        function showSlide(index) {
-            if (index >= totalSlides) currentSlide = 0;
-            else if (index < 0) currentSlide = totalSlides - 1;
-            else currentSlide = index;
-
-            const slidesContainer = document.querySelector('.slides');
-            slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-
-            dots.forEach(dot => dot.classList.remove('active'));
-            dots[currentSlide].classList.add('active');
+        // Function to show slides
+    function showSlide(index) {
+        if (index >= totalSlides) {
+            currentSlide = 0;
+        } else if (index < 0) {
+            currentSlide = totalSlides - 1;
+        } else {
+            currentSlide = index;
         }
+
+        const slidesContainer = document.querySelector('.slides');
+        slidesContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+
+        // Update indicators
+        dots.forEach(dot => dot.classList.remove('active'));
+        dots[currentSlide].classList.add('active');
+    }
 
         function nextSlide() { showSlide(currentSlide + 1); }
         function prevSlide() { showSlide(currentSlide - 1); }
